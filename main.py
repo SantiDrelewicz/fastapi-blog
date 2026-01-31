56b325560c8fc1bfd49906817bc8eafcfd52088f
SantiDrelewicz <santidrelewicz2000@gmail.com>
2026-01-31 19:11:10 -0300

    02 - Templates

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
 
 POSTS: list[dict[str, int | str]] = [
     {
@@ -23,12 +25,14 @@ POSTS: list[dict[str, int | str]] = [
 ]
 
 
-
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{POSTS[0]['title']}</h1>"
-
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": POSTS, "title": "Home"},
+    )
 
 @app.get("/api/posts")
 def get_posts():
